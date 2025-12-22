6217c4116e0398474263cbe13389dc2474f74a91
pablocastillo4821@gmail.com <pablocastillo4821@gmail.com>
2025-12-21 23:56:36 -0600

    primer commit

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Button, Pressable } from 'react-native';
+
+const icon = require('./assets/icon.png');
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
+      <Pressable onPress={() => alert('Hola')}>
+        <Image source={icon} style={{ width: 100, height: 100 }} />
+      </Pressable>
+      <Text style={styles.text}>
+        ¡Hola! Este es mi aplicación de expo-cli
+      </Text>
     </View>
   );
 }
@@ -13,8 +20,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingHorizontal: 20
   },
+  text: {
+    fontSize: 12.5,
+    width: '100%',
+    textAlign: 'center', // Alinea el texto al centro
+    color: '#fff' // Color del texto
+  }
 });
